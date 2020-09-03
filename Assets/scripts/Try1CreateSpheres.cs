ffdc3b1285a92d43ad812274e4ac5ea0c5cd29eb
fd3yt4 <fd3yt4@localhost.localdomain>
2020-09-03 12:30:20 +0300

    Initial upload

---
+++ Assets/scripts/Try1CreateSpheres.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Try1CreateSpheres : MonoBehaviour
+{
+	private GameObject s;
+	private Rigidbody rb;
+	private int counter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	if (counter <= 75) 
+    	{
+    		counter++;
+    		if (s.transform.position.y <= 0.5)
+			Destroy(s);
+    	} else {
+    		counter = 0;
+    		s = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		s.transform.position = new Vector3 (((Random.value*50) % 8) - ((Random.value*50) % 8), 5, ((Random.value*50) % 8) - ((Random.value*50) % 8));
+		rb = s.gameObject.AddComponent(typeof(Rigidbody)) as Rigidbody;
+	//yield return new WaitForSecondsRealtime(5);
+    	}      
+    }
+}
---
+++ Assets/scripts/Try1Cube.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Try1Cube : MonoBehaviour
+{
+	
+	Rigidbody rb;
+	float xInput;
+	float zInput;
+	int points;
+	public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    	if (Input.GetKeyDown(KeyCode.Space))
+    	{
+        	rb.AddForce(Vector3.up * 200);
+        }
+        
+        if (gameObject.transform.position.y <= -1)
+			Destroy(gameObject);
+        
+        xInput = Input.GetAxis("Horizontal");
+        zInput = Input.GetAxis("Vertical");
+        
+        rb.AddForce(xInput * speed, 0, zInput * speed);
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+    	if(collision.gameObject.tag != "MyPlane")
+    	{
+    		points++;
+    		Destroy(collision.gameObject);
+    	}
+    }
+}
